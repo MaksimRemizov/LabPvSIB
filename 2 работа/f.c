fbd4256fe043531fff0191e380776712c9465edb
MaksimRemizov <agoro002@mail.ru>
2021-09-23 22:20:58 +0300

    2work fix

--- 2 работа/f.c
+++ 2 работа/f.c
@@ -2,5 +2,6 @@
 #include <math.h>
 double f(double x)
 {
-    return ((x <= 3) ? (x*x + 3*x + 9) : (sin(x)/(x*x-9)));
+    return (x <= 3) ? (x*x + 3*x + 9) : (sin(x)/(x*x-9));
 }
+
--- 2 работа/isInArea.c
+++ 2 работа/isInArea.c
@@ -3,7 +3,7 @@ _Bool isInArea (double x, double y){
     if (((x <= 1 && x >= -1) && (y >= 0 && y <= 1)) || 
     ((x <= 0 && x >= -1) && (y >= -1 && y <= 0)))
     {
-         return (1);
+         return 1;
     }
-    return (0);
+    return 0;
 }
