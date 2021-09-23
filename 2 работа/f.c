50a4971a7288cb140e0275a49c29338b7f41207f
MaksimRemizov <agoro002@mail.ru>
2021-09-23 18:13:00 +0300

    2Work fix

--- 2 работа/f.c
+++ 2 работа/f.c
@@ -1,5 +1,6 @@
 #include "func.h"
 #include <math.h>
-double f(double x){
+double f(double x)
+{
     return ((x <= 3) ? (x*x + 3*x + 9) : (sin(x)/(x*x-9)));
 }
--- 2 работа/main.c
+++ 2 работа/main.c
@@ -1,30 +1,32 @@
 #include "func.h"
-int main()
+#include <stdlib.h>
+int main(void)
 {
     int operator;
     printf("Input task(1 or 2)= ");
     scanf("%d", &operator);
     double x, y;
-     switch(operator){
+     switch(operator)
+	 {
         case 1:
             printf("Input x=");
             scanf("%lf",&x);
             printf("Input y=");
             scanf("%lf",&y);
             if (isInArea(x,y))
-                printf("Point is in area");
+                printf("Point is in area\n");
             else
-                printf("Point is in outside of an area");
+                printf("Point is in outside of an area\n");
             break;                 
             
         case 2:            
             printf("Input x=");
             scanf("%lf",&x); 
-            printf("%lf ", f(x));
+            printf("%lf\n", f(x));
             break;
         default: 
-            printf("Error");
+            printf("Error\n");
             break;
     }
-    return (0);
+	system("pause");
 }
