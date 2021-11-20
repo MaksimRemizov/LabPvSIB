7da7a8ea2535ed3fd04ed8d1cfd34863ff0463ef
MaksimRemizov <agoro002@mail.ru>
2021-11-20 21:33:18 +0300

    1

---
+++ 5 работа/task1.c
@@ -0,0 +1,11 @@
+unsigned int strlen(const char *str)
+{
+	int len = 0;
+	while(*str++) ++len;
+	return len;
+}
+
+
+
+
+
---
+++ 5 работа/task5.c
@@ -0,0 +1,13 @@
+
+#include "func.h" 
+void strcat(char *dest, int len, const char *first, const char *second)
+{
+	strcpy(dest, len, first);
+	unsigned firstLen =  strlen(first);
+	strcpy(dest + firstLen, len - firstLen, second);
+}
+
+
+
+
+
